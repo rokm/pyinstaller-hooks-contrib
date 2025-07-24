d8505e33983bbf7b750b17c8525573b34fff804d
Rok Mandeljc <rok.mandeljc@gmail.com>
2025-07-25 00:31:30 +0200

    Adjust the automatically-raised recursion limit value for python < 3.11 on Windows
    The recursion limit test in the main PyInstaller repository seems to
    crash the python process when ran under Windows or Cygwin with
    python < 3.11 and recursion limit is set to > 2097.
    
    Therefore, for the affected combinations of platform and python version,
    adjust the automatically-raised recursion limit value to a conservative
    value of 2000. For other combinations, continue using the value of 5000.

--- _pyinstaller_hooks_contrib/__init__.py
+++ _pyinstaller_hooks_contrib/__init__.py
@@ -12,6 +12,8 @@
 
 import sys
 
+from PyInstaller import compat
+
 __version__ = '2025.7'
 __maintainer__ = 'Legorooj, bwoodsend'
 __uri__ = 'https://github.com/pyinstaller/pyinstaller-hooks-contrib'
@@ -32,6 +34,13 @@ def get_hook_dirs():
 # installed in the environment, and their analysis typically requires recursion limit that exceeds the default 1000.
 # Therefore, automatically raise the recursion limit to at least 5000. This alleviates the need to do so on per-hook
 # basis.
-new_recursion_limit = 5000
+if (compat.is_win or compat.is_cygwin) and not compat.is_py311:
+    # The recursion limit test in PyInstaller repository seems to be crashing python interpreter under Windows and
+    # Cygwin with python < 3.11 when recursion limit is raised to 5000. In fact, with python 3.8, the crash seems
+    # to occur if recursion limit is set above 2097. So for combinations of affected platforms and python versions,
+    # use more conservative value for the new recursion limit.
+    new_recursion_limit = 2000
+else:
+    new_recursion_limit = 5000
 if sys.getrecursionlimit() < new_recursion_limit:
     sys.setrecursionlimit(new_recursion_limit)
